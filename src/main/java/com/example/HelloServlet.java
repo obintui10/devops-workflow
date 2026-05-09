7c9f8ca0ae537238b92271849f2b9030bab6c499
Obi Ayuk Ntui <ayukspecial@gmail.com>
2026-05-10 00:52:46 +0530

    Create HelloServlet.java

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h2>Hello from DevOps Workflow!</h2>");
+        out.println("</body></html>");
+    }
+}
